e922fd45568041d4163dfeeccf2df0331abcced9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-06-23 10:18:52 +0100

    projet de base

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialAp(home: HomePage()));
+  runApp(const MaterialApp(home: HomePage()));
 }
 
 class HomePage extends StatefulWidget {
